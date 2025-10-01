861e39494b4a44cf780acfc2e144cd1cc0d6e3fe
zakia <eellyaaviolyn@gmail.com>
2025-10-01 08:10:24 +0700

    Add files via upload

---
+++ foreach.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $array = array('PHP','Python','Ruby','vinzkie','eellyaa');
+    foreach($array as $value){
+        echo $value.'<br>';
+    }
+    ?>
+</body>
+</html>
